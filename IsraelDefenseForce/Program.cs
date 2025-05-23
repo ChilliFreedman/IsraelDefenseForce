6e2180074a983c6ed15155e40ab401cecf9e31fa
chili <ymf21971@gmail.com>
2025-05-23 15:13:41 +0300

    Filled the base classes with their fields and created the base instances in Main.

--- IsraelDefenseForce/Program.cs
+++ IsraelDefenseForce/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace IsraelDefenseForce
 {
@@ -10,40 +13,70 @@ namespace IsraelDefenseForce
     {
         static void Main(string[] args)
         {
-            IDF.CurrentCommander = "eyal";
-            Hamas.CurrentCommander = "sinwar";
-            Console.WriteLine(IDF.CurrentCommander);
-            Console.WriteLine(IDF.GetDateOfEstablishment());
+            //Mohammed,Ahmed,Ali,Omar,Yousef,Amina,Fatima,Noor,Salim,Layla
+            //knife,gun,m16,ak47
+            //List<string> TerorisNames = new List<string>() { "Mohammed", "Ahmed", "Ali", "Omar", "Yousef", "Amina", "Fatima", "Noor", "Salim", "Layla" };
+
+            Dictionary<string, int> dicMohammed = new Dictionary<string, int>() { { "gun", 2 }, { "knife", 2 } };
+            Terrorist Mohammed = new Terrorist("Mohammed", 3, true,dicMohammed);
+            Hamas.ListOfTerrorist.Add(Mohammed);
+
+            Dictionary<string, int> dicAhmed = new Dictionary<string, int>() { { "ak47", 1 }};
+            Terrorist Ahmed = new Terrorist("Ahmed", 3, true, dicAhmed);
+            Hamas.ListOfTerrorist.Add(Ahmed);
+
+            Dictionary<string, int> dicAli = new Dictionary<string, int>() { { "gun", 3 } };
+            Terrorist Ali = new Terrorist("Ali", 3, true, dicAli);
+            Hamas.ListOfTerrorist.Add(Ali);
+
+            Dictionary<string, int> dicOmar = new Dictionary<string, int>() { { "m16", 1 } };
+            Terrorist Omar = new Terrorist("Omar", 3, true, dicOmar);
+            Hamas.ListOfTerrorist.Add(Omar);
+
+            Dictionary<string, int> dicYousef = new Dictionary<string, int>() { { "ak47", 2 }, { "m16", 1 } };
+            Terrorist Yousef = new Terrorist("Yousef", 3, true, dicYousef);
+            Hamas.ListOfTerrorist.Add(Yousef);
+
+            F16FighterJet f16 = new F16FighterJet();
+            M109Artillery m109 = new M109Artillery();
+            Hermes460Drone zic = new Hermes460Drone();
+            IDF.ListOfStrikUnits  = new List<StrikeUnit> { f16, m109, zic };
+
+            Console.WriteLine(IDF.ListOfStrikUnits.Count);
+            Console.WriteLine(Mohammed.Rank);
+            Console.WriteLine(Hamas.ListOfTerrorist.Count);
             Console.WriteLine(Hamas.CurrentCommander);
-            Console.WriteLine(Hamas.GetDateOfEstablishment());
-            List<Dictionary<string, int>> listi = new List<Dictionary<string, int>>() ;
 
-            
-
-
-
-            Dictionary<string,int> dic = new Dictionary<string, int>() { { "aa", 2 }, { "hh", 2 } };
-            Terrorist muhamad = new Terrorist("muhamad", 4, "alive",dic);
-            Terrorist ahmed = new Terrorist("ahmed", 3, "alive", dic);
-            Terrorist sinwar = new Terrorist("ahmed", 2, "alive", dic);
-            Terrorist abu = new Terrorist("ahmed", 5, "ded", dic);
-            Terrorist salim = new Terrorist("ahmed", 1, "alive", dic);
-            Hamas.ListOfTerrorist = new List<Terrorist>() { muhamad,ahmed};
-            TerroristReport report1 = new TerroristReport(muhamad,"home","12/09/12");
-            TerroristReport report2 = new TerroristReport(muhamad, "home", "12/09/12");
-            TerroristReport report3 = new TerroristReport(muhamad, "home", "12/09/12");
-            TerroristReport report4 = new TerroristReport(muhamad, "home", "12/09/12");
-            TerroristReport report5 = new TerroristReport(muhamad, "home", "12/09/12");
-            F16FighterJet a = new F16FighterJet();
-            Hermes460Drone b = new Hermes460Drone();
-            M109Artillery c = new M109Artillery();
-            
-            Console.WriteLine($"name strik: {a.UnicName}. strik left: {a.StrikLeft}:");
-            Console.WriteLine($"name strik: {b.UnicName}. strik left: {b.StrikLeft}:");
-            Console.WriteLine($"name strik: {c.UnicName}. strik left: {c.StrikLeft}:");
-
-            Console.WriteLine(report5.TerroristInformition.Name);
-
+            //יצירת דיווחים של אמן
+            TerroristReport report1 = new TerroristReport(Mohammed, "home", DateTime.Now);
+            TerroristReport report2 = new TerroristReport(Yousef, "outside", DateTime.Now);
+            TerroristReport report3 = new TerroristReport(Ali, "car", DateTime.Now);
+            TerroristReport report4 = new TerroristReport(Mohammed, "home", DateTime.Now);
+            TerroristReport report5 = new TerroristReport(Ali, "home", DateTime.Now);
+            TerroristReport report6 = new TerroristReport(Mohammed, "home", DateTime.Now);
+            TerroristReport report7 = new TerroristReport(Ahmed, "car", DateTime.Now);
+            TerroristReport report8 = new TerroristReport(Mohammed, "home", DateTime.Now);
+            TerroristReport report9 = new TerroristReport(Omar, "home", DateTime.Now);
+            TerroristReport report10 = new TerroristReport(Ahmed, "outsaide", DateTime.Now);
+            //הכנסה לרשימה של אמן
+            IntelligenceUnit.ListReports = new List<TerroristReport>()
+            { report1,report2,report3,report4,report5,report6,report7,report8,report9,report10};
+            foreach (TerroristReport report in IntelligenceUnit.ListReports)
+            {
+                Console.WriteLine(report.TerroristInformition.Name);
+            }
+            //קורא לפונקציה שבודקת איזה מחבל יש הכי הרבה פעמים
+            Console.WriteLine( IntelligenceUnit.GetTheterWithMostReports(IntelligenceUnit.ListReports));
+            //קורא לפונקציה של
+            //הצגת כלי תקיפה זמינים ואת שארית התחמושת שלהם
+            IDF.getlistofunitswithstrikleft();
+            Console.WriteLine(f16.StrikLeft);
+            //foreach(StrikeUnit s in IDF.ListOfStrikUnits)
+            //{
+            //    Console.WriteLine(s.UnicName);
+            //    //Console.WriteLine(s);
+            //}
+            //Console.WriteLine(f16.UnicName);
             //IDF.GetDateOfEstablishment();
         }
     }
--- IsraelDefenseForce/Terrorist.cs
+++ IsraelDefenseForce/Terrorist.cs
@@ -10,15 +10,15 @@ namespace IsraelDefenseForce
     {
         public string Name;
         public int Rank;
-        public string LifeStatus;
+        public bool IsAlive;
         public Dictionary<string,int> Weapons;
 
         
-        public Terrorist(string name,int rank, string lifeStatus,Dictionary<string, int> wepons)
+        public Terrorist(string name,int rank, bool isAlive, Dictionary<string, int> wepons)
         {
             this.Name = name;
             this.Rank = rank;
-            this.LifeStatus = lifeStatus;
+            this.IsAlive = isAlive;
             this.Weapons = wepons;
         }
        
